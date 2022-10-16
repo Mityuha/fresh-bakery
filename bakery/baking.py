601a5439f636c7590d887cf1feda0f42aaaccce1
mityuha <mit.makaroff@gmail.com>
2022-10-16 14:55:04 +0400

    add new function: check if object is cake or piece of cake

--- bakery/baking.py
+++ bakery/baking.py
@@ -23,14 +23,14 @@ from .stuff import (
     flatten,
     is_baked,
     is_cake,
-    is_piece_of_cake,
+    is_cake_or_piece,
     replace_cakes,
 )
 
 
 def determine_baking_method(recipe: Any) -> BakingMethod:
     """Determine the first available baking method for recipe."""
-    if is_cake(recipe) or is_piece_of_cake(recipe):
+    if is_cake_or_piece(recipe):
         # Baking method depends on what object
         # really is inside cake/piece_of_cake
         return BakingMethod.BAKE_AUTO
@@ -163,7 +163,7 @@ class Ingredients:
                 await bake(recipe)
 
         recipe = self.recipe
-        if is_cake(recipe) or is_piece_of_cake(recipe):
+        if is_cake_or_piece(recipe):
             recipe = self.recipe()
 
         if not self.cake_baking_method:
